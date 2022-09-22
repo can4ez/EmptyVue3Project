28bf9bcdfdc218d020cb6b912a0960a8c969e375
Alex <schein.sascha2013@mail.ru>
2022-09-22 13:10:33 +0500

    Init vue3

---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- Главная страница -->
+  <div id="homeView">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+    }
+  },
+  components: {
+  },
+  computed: {},
+  methods: {
+  },
+  mounted() {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
